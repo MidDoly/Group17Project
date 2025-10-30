824d0e28aa307e4e915379abafeb28fa3d0a589d
MidDoly <apexenglishcentera4@gmail.com>
2025-10-30 19:37:54 +0700

    Player Move & Jump

---
+++ Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,63 @@
+
+using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+public class PlayerControll : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5.0f;
+    public float jumpStartingSpeed = 3.0f;
+    public float fallingAcceleration = 0.98f;
+    private const float groundPosition = 0f;
+    public InputSystem_Actions playerControls;
+    UnityEngine.Vector2 moveDirection = UnityEngine.Vector2.zero;
+    Vector2 playerVelocity = Vector2.zero;
+    private InputAction move;
+    private InputAction fire;
+    private InputAction jump;
+    private float ySpeed = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        playerControls = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+        fire = playerControls.Player.Attack;
+        fire.Enable();
+        jump = playerControls.Player.Jump;
+        jump.Enable();
+    }
+    
+    void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+        jump.Disable();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jump.ReadValue<float>() != 0 && rb.position.y <= groundPosition) ySpeed = jumpStartingSpeed;
+        else if (rb.position.y > groundPosition) ySpeed -= fallingAcceleration;
+        else ySpeed = 0; 
+        moveDirection = move.ReadValue<UnityEngine.Vector2>();
+        
+        if (moveDirection.x == 0) playerVelocity = new Vector2(0, ySpeed);
+        else playerVelocity = new Vector2(moveDirection.x / Math.Abs(moveDirection.x) * moveSpeed, ySpeed);
+    }
+    
+    void FixedUpdate()
+    {
+        rb.linearVelocity = playerVelocity;
+    }
+}
